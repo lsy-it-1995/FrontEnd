c44e3e279625b5e5217c14640a945af64d9c0b33
lsy-it-1995 <garyshaoyu@gmail.com>
2021-08-02 20:38:59 +0800

    react

--- React 尚硅谷IT培训学校/self/react_staging/src/App.jsx
+++ React 尚硅谷IT培训学校/self/react_staging/src/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
+    getStudent = () =>{
+        axios.get('http://localhost:5000/students').then(
+            response =>{console.log('success', response.data);},
+            error => {console.log('fail', error);}
+        )
+    }
     render() {
         return (
             <div>
-                App...
+                <button onClick={this.getStudent}>Click for student</button>
             </div>
         )
     }
