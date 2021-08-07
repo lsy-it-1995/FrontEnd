e6f390a48818bb007546c1a5d95b98e5421bec53
lsy-it-1995 <garyshaoyu@gmail.com>
2021-08-07 10:49:15 +0800

    101 尚硅谷 react教程 求和案例 redux完整版

---
+++ React 尚硅谷IT培训学校/self/redux-test/srcFullFiles/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Count from './component/Count'
+export default class app extends Component {
+    render() {
+        return (
+            <div>
+                <Count />
+            </div>
+        )
+    }
+}
