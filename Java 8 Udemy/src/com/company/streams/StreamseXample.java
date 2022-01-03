08ad24de8ba54e08a949d801d9a620025f2062f8
lsy-it-1995 <garyshaoyu@gmail.com>
2022-01-02 16:13:30 -0800

    streams

---
+++ Java 8 Udemy/src/com/company/streams/StreamseXample.java
@@ -0,0 +1,22 @@
+package com.company.streams;
+
+import com.company.data.Student;
+import com.company.data.StudentDataBase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StreamseXample {
+    public static void main(String[] args) {
+
+        Predicate<Student> studentGradePredicate = (s) -> s.getGradeLevel() >= 3;
+        Predicate<Student> studentGPAPredicate = (s) -> s.getGpa() >= 3.9;
+        Map<String, List<String>> map = StudentDataBase.getAllStudents().stream()
+                                        .filter(studentGradePredicate)
+                                        .filter(studentGPAPredicate)
+                                        .collect(Collectors.toMap(Student::getName, Student::getActivities));
+        System.out.println(map);
+    }
+}
