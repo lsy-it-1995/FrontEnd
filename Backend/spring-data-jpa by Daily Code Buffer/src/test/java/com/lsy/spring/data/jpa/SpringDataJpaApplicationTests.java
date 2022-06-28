c89b53fbca89923312df100bffe2c17114493bf9
LSY <61727133+lsy-it-1995@users.noreply.github.com>
2022-06-28 20:21:38 +0800

    update todolist udemy

---
+++ Backend/spring-data-jpa by Daily Code Buffer/src/test/java/com/lsy/spring/data/jpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsy.spring.data.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
